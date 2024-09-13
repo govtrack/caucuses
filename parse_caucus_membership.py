4a9fa1211438c246b5fc66417c3dd1a6219e3bd8
Joshua Tauberer <jt@occams.info>
2024-09-13 11:41:45 -0400

    more

--- parse_caucus_membership.py
+++ parse_caucus_membership.py
@@ -3,68 +3,158 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 import datetime
+from collections import defaultdict
+import unicodedata
+
+
+headers = {
+    'User-Agent': 'Mozilla',
+}
+
 
 class LegislatorInfo:
-	def __init__(self):
-		# Load current legislators.
-		self.legislators_current = rtyaml.load(open("../congress-legislators/legislators-current.yaml"))
+  def __init__(self):
+    # Load current legislators.
+    self.legislators_current = rtyaml.load(open("../congress-legislators/legislators-current.yaml"))
 
-		# Make a map from bioguide ID to a summary to the legislator dict.
-		self.id_map = { }
-		for p in self.legislators_current:
-			self.id_map[p['id']['bioguide']] = p
+    # Make a map from bioguide ID to a summary to the legislator dict.
+    self.id_map = { }
+    for p in self.legislators_current:
+      self.id_map[p['id']['bioguide']] = p
 
-		# Make a map from legislator website URL domains to
-		# bioguide IDs.
-		self.website_map = { }
-		for p in self.legislators_current:
-			url = p['terms'][-1]['url']
-			url = urlparse(url).netloc
-			self.website_map[url] = p['id']['bioguide']
+    # Make a map from legislator names to IDs.
+    self.name_map = { }
+    for p in self.legislators_current:
+      name = p['name']['official_full']
+      self.name_map[name] = p['id']['bioguide']
+      self.name_map[u"".join(c for c in unicodedata.normalize('NFKD', name)
+                             if not unicodedata.combining(c))] = p['id']['bioguide']
 
-def new_democrat_coalition(cache):
-	# Scan the New Dems website member list for links to
-	# legislator pages.
-	res = requests.get("https://newdemocratcoalition.house.gov/members")
-	soup = BeautifulSoup(res.text, 'html.parser')
-	newdems = set()
-	for node in soup.find_all('a'):
-		href = node.get('href')
-		if href:
-			href = urlparse(href).netloc
-			if "house.gov" in href.lower() and href != "newdemocratcoalition.house.gov":
-				if href not in cache.website_map:
-					print("Failed to map legislator from", href)
-				else:
-					newdems.add(cache.website_map[href])
-	return newdems
+    # Make a map from legislator names to IDs.
+    self.last_name_map = defaultdict(lambda : set())
+    for p in self.legislators_current:
+      self.last_name_map[p['name']['last']].add(p['id']['bioguide'])
+
+    # Make a map from legislator website URL domains to
+    # bioguide IDs.
+    self.website_map = { }
+    for p in self.legislators_current:
+      url = p['terms'][-1]['url']
+      url = urlparse(url).netloc
+      self.website_map[url] = p['id']['bioguide']
+
+
+def scrape_for_legislator_homepage_links(url, exclude_urls, cache):
+  # Scan the New Dems website member list for links to
+  # legislator pages.
+  res = requests.get(url)
+  soup = BeautifulSoup(res.text, 'html.parser')
+  members = set()
+  for node in soup.find_all('a'):
+    href = node.get('href')
+    if href:
+      href = urlparse(href).netloc
+      if "house.gov" in href.lower() and href not in exclude_urls:
+        if href not in cache.website_map:
+          print("Failed to map legislator from", href, "in", url)
+        else:
+          members.add(cache.website_map[href])
+  return members
+
+
+def republican_study_committee(cache):
+  res = requests.get("https://rsc-hern.house.gov/about/membership")
+  soup = BeautifulSoup(res.text, 'html.parser')
+  members = set()
+  for node in soup.select('.each-member-name'):
+    name = node.string\
+      .replace("Rep. ", "")\
+      .replace("Chairman ", "")\
+      .replace("Speaker ", "")\
+      .replace("Majority Leader ", "")\
+      .replace("Minority Leader ", "")\
+      .replace("  ", " ")\
+      .strip()
+    if name in cache.name_map:
+      members.add(cache.name_map[name])
+      continue
+    last_name = name.split(" ")[-1]
+    if last_name in cache.last_name_map and len(cache.last_name_map[last_name]) == 1:
+      members.add(list(cache.last_name_map[last_name])[0])
+      continue
+    print("Failed to map RSC legislator from", name)
+  return members
+
+
+def republican_governance_group(cache):
+  res = requests.get("https://republicangovernance.com/", headers=headers)
+  soup = BeautifulSoup(res.text, 'html.parser')
+  members = set()
+  for node in soup.select('.genlist li'):
+    if not node.string: continue
+    name = node.string\
+      .replace("Congressman ", "")\
+      .replace("Congresswoman ", "")\
+      .strip()
+    if name in cache.name_map:
+      members.add(cache.name_map[name])
+      continue
+    last_name = name.split(" ")[-1]
+    if last_name in cache.last_name_map and len(cache.last_name_map[last_name]) == 1:
+      members.add(list(cache.last_name_map[last_name])[0])
+      continue
+    print("Failed to map RGG legislator from", name)
+  return members
 
 
 def save_caucus(caucus_name, filename, members, cache):
-	# Sort members.
-	members = list(members)
-	members.sort()
+  # Sort members.
+  members = list(members)
+  members.sort()
 
-	# Reformat.
-	members = [
-		{
-			"id": key,
-			"name": cache.id_map[key]["name"]["official_full"]
-		}
-	 	for key in members ]
+  # Reformat.
+  members = [
+    {
+      "id": key,
+      "name": cache.id_map[key]["name"]["official_full"]
+    }
+    for key in members ]
 
-	with open(filename, "w") as f:
-		rtyaml.dump({
-		            "name": caucus_name,
-		            "updated": datetime.datetime.now().isoformat(),
-		            "members": members }, f)
+  with open(filename, "w") as f:
+    rtyaml.dump({
+                "name": caucus_name,
+                "updated": datetime.datetime.now().isoformat(),
+                "members": members }, f)
 
 
 if __name__ == "__main__":
-	# Pre-load some legislator info.
-	cache = LegislatorInfo()
-	save_caucus(
-	            "New Democrat Coalition",
-	            "newdemocratcoalition.yaml",
-				new_democrat_coalition(cache),
-				cache)
+  # Pre-load some legislator info.
+  cache = LegislatorInfo()
+  save_caucus(
+              "Republican Study Committee",
+              "republicanstudy.yaml",
+              republican_study_committee(cache),
+              cache)
+  save_caucus(
+              "Republican Governance Group",
+              "republicangovernance.yaml",
+              republican_governance_group(cache),
+              cache)
+  save_caucus(
+              "New Democrat Coalition",
+              "newdems.yaml",
+              scrape_for_legislator_homepage_links("https://newdemocratcoalition.house.gov/members",
+                                                   { "newdemocratcoalition.house.gov" }, cache),
+              cache)
+  save_caucus(
+              "Congressional Progressive Caucus",
+              "congressionalprogressive.yaml",
+              scrape_for_legislator_homepage_links("https://progressives.house.gov/caucus-members",
+                                                   { "" }, cache),
+              cache)
+  save_caucus(
+              "Problem Solvers Caucus",
+              "problemsolvers.yaml",
+              scrape_for_legislator_homepage_links("https://problemsolverscaucus.house.gov/caucus-members", 
+                                                   { "www.house.gov" }, cache),
+              cache)
---
+++ vote_analysis.py
@@ -0,0 +1,210 @@
+import csv
+import rtyaml
+import glob
+from collections import defaultdict
+
+# Load ideology scores and party
+ideology = { }
+party = { }
+for line in csv.DictReader(open("sponsorshipanalysis_h.txt")):
+  ideology[int(line["ID"])] = float(line["ideology"])
+  party[int(line["ID"])] = line["party"]
+
+# Load bioguide ID to govtrack ID mapping.
+legislators_current = rtyaml.load(open("../congress-legislators/legislators-current.yaml"))
+id_map = { }
+for p in legislators_current:
+  id_map[p['id']['bioguide']] = p['id']['govtrack']
+
+# Load caucus memberships.
+caucus_membership = set()
+for caucus in glob.glob("*.yaml"):
+  caucusdata = rtyaml.load(open(caucus))
+  caucus = caucus.replace(".yaml", "")
+  for p in caucusdata["members"]:
+    caucus_membership.add((caucus, id_map[p["id"]]))
+all_caucuses = set(x[0] for x in caucus_membership)
+
+
+# Make a map from legislator to Census region.
+census_regions = {
+  "Northeast": { "CT", "ME", "MA", "NH", "RI", "VT", "NJ", "NY", "PA" },
+  "Midwest": { "IN", "IL", "MI", "OH", "WI", "IA", "KS", "MN", "MO", "NE", "ND", "SD" },
+  "South": { "DE", "DC", "FL", "GA", "MD", "NC", "SC", "VA", "WV", "AL", "KY", "MS", "TN", "AR", "LA", "OK", "TX "},
+  "West": { "AZ", "CO", "ID", "NM", "MT", "UT", "NV", "WY", "AK", "CA", "HI", "OR", "WA" },
+}
+census_region_map = { }
+for p in legislators_current:
+  state = p["terms"][-1]["state"]
+  for region, states in census_regions.items():
+    if state in states:
+      break
+  else:
+    region = None
+  census_region_map[p['id']['govtrack']] = region
+
+
+# Run regressions
+
+def regression(title, X, Y, exclude_features = set()):
+  # Transform X from a list of dicts of factors to
+  # a matrix with a defined order. Also transform
+  # True to 1, False to -1, and None to 0. (We don't
+  # do the same for Y because Logit requires Y in
+  # [0, 1].)
+  def to_number(value):
+    if value is True: return 1
+    if value is False: return -1
+    if value is None: return 0
+    return value
+  from itertools import chain
+  features = set(chain.from_iterable(set(x.keys()) for x in X))
+  features -= exclude_features
+  if len(features) == 0:
+    return None
+  features = sorted(features)
+  X = [
+    [to_number(x[key]) for key in features]
+    for x in X
+  ]
+
+  # Run regression.
+
+  #from statsmodels.api import OLS
+  #model = OLS(Y, X)
+
+  from statsmodels.discrete.discrete_model import Logit
+  import numpy.linalg
+  model = Logit(Y, X)
+  import warnings
+  with warnings.catch_warnings():
+    warnings.filterwarnings("ignore", message="Perfect separation")
+    warnings.filterwarnings("ignore", message="divide by zero encountered")
+    warnings.filterwarnings("ignore", message="overflow encountered in exp")
+    try:
+      fit = model.fit(maxiter=100, disp=False, warn_convergence=False) # disp silences stdout
+    except numpy.linalg.LinAlgError:
+      return None
+  if not fit.converged: return None
+
+  # Transform results back into a dictionary.
+  fit = {
+    "title": title,
+    "rsquared": float(fit.prsquared), # .rsquared for OLS
+    "features": {
+      feature: {
+        "value": float(fit.params[i]),
+        "pvalue": float(fit.pvalues[i])
+      }
+      for i, feature in enumerate(features)
+    }
+  }
+
+  return fit
+
+
+def fit_best_model(title, X, Y):
+  # Remove features iteratively from the model
+  # that are non-significant 
+  exclude_features = set()
+  repeat = True
+  last_fit_model = None
+  while repeat:
+    repeat = False
+    model = regression(title, X, Y, exclude_features=exclude_features)
+    if model is None:
+      break
+    last_fit_model = model
+    for feature, params in model["features"].items():
+      if feature == "intercept": continue # don't remove this one
+      if params["pvalue"] > .01:
+        exclude_features.add(feature)
+        repeat = True
+        break # only remove one feature at a time
+  return last_fit_model
+
+def predict_ideology():
+  Y = [ ]
+  X = [ ]
+  for id in ideology:
+    Y.append(ideology[id])
+    X.append({
+             "intercept": 1,
+             "party_Republican": party[id] == "Republican",
+             }
+             |
+             {
+              "caucus_" + caucus: (caucus, id) in caucus_membership
+              for caucus in all_caucuses
+             })
+  regression("ideology", X, Y)
+
+
+def predict_all_votes():
+  # Predict across all vote data (which is weird because
+  # in some votes left will go toward Aye and in others
+  # toward No, but since Rs have the majority probably it'll
+  # mostly be No.).
+  Y = [ ]
+  X = [ ]
+  for row in csv.DictReader(open("votes_118_house.csv")):
+    vote = row["vote"]
+    if vote not in ("Aye", "Yea", "No", "Nay"): continue
+    vote = vote in ("Aye", "Yea")
+
+    id = int(row["person"])
+
+    Y.append(vote)
+    X.append({
+             "intercept": 1,
+             "ideology": ideology[id],
+             "party_Republican": party[id] == "Republican",
+             }
+             |
+             {
+              "caucus_" + caucus: (caucus, id) in caucus_membership
+              for caucus in all_caucuses
+             })
+  regression("vote", X, Y)
+
+
+# Fit individual votes
+def load_all_votes():
+  all_votes = defaultdict(lambda : [])
+  for row in csv.DictReader(open("votes_118_house.csv")):
+    vote = row["vote"]
+    if vote not in ("Aye", "Yea", "No", "Nay"): continue
+    vote = vote in ("Aye", "Yea")
+    id = int(row["person"])
+    vote_id = ":".join(row[key] for key in ("congress", "session", "chamber", "number"))
+    all_votes[vote_id].append((id, vote))
+  return all_votes.items()
+def predict_vote(vote_id, votes):
+  X = [ ]
+  Y = [ ]
+  for id, vote in votes:
+    Y.append(vote)
+    X.append({
+             "intercept": 1,
+             "ideology": ideology[id],
+             "party_Republican": party[id] == "Republican",
+             }
+             |
+             {
+              "censusregion_" + region: census_region_map.get(id) == region
+              for region in census_regions
+             }
+             |
+             {
+              "caucus_" + caucus: (caucus, id) in caucus_membership
+              for caucus in all_caucuses
+             }
+             )
+  return fit_best_model(vote_id, X, Y)
+for vote_id, votes in load_all_votes():
+  model = predict_vote(vote_id, votes)
+  if model:
+    print(rtyaml.dump(model))
+    print()
+
+
